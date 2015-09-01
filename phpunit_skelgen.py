bae14fa89cabecb15bb700a2f2d60f7cd89ce88a
Jamal Wilman <jamal.silva@aker.com.br>
2015-09-01 17:55:20 -0300

    [add] Settings class
    Adds Settings class responsible for loading the plugin and project
    specific options.

---
+++ phpunit_skelgen.py
@@ -0,0 +1,22 @@
+import sublime, sublime_plugin
+
+class Settings():
+    view = None
+    settings = None
+
+    def __init__(self, view):
+        self.settings = sublime.load_settings('phpunit_skelgen.sublime-settings')
+        self.project_settings = {}
+
+        if sublime.active_window() is not None:
+            project_settings = view.settings()
+            if project_settings.has("phpunit_skelgen"):
+                self.project_settings = project_settings.get('phpunit_skelgen')
+
+    def get(self, key):
+        if key in self.project_settings:
+            return self.project_settings.get(key)
+
+        return self.settings.get(key)
+
+
