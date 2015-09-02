7794d64aca446191e380b1dbb7280125ca303ce6
Jamal Wilman <jamal.silva@aker.com.br>
2015-09-02 17:07:52 -0300

    [add] class and methods comments
    Adds class and methods docstrings

--- phpunit_skelgen.py
+++ phpunit_skelgen.py
@@ -4,9 +4,17 @@ import os.path
 import subprocess
 
 class Settings():
+    """
+    Class that loads the necessary setting for running the generate_test
+    command.
+    """
+
     settings = None
 
     def __init__(self, view):
+        """
+        Loads plugin and project specific settings
+        """
         self.settings = sublime.load_settings('phpunit_skelgen.sublime-settings')
         self.project_settings = {}
 
@@ -16,6 +24,10 @@ class Settings():
                 self.project_settings = project_settings.get('phpunit_skelgen')
 
     def get(self, key):
+        """
+        If defined, gets the project specific settings. If not, tries the plugin
+        settings.
+        """
         if key in self.project_settings:
             return self.project_settings.get(key)
 
@@ -23,8 +35,22 @@ class Settings():
 
 
 class GenerateTestCommand(sublime_plugin.TextCommand):
+    """
+    Defines the sublime text command
+
+    Can be tested at the console:
+    - view.run_command('generate_test')
+
+    Or binding a key combination at Preferences => Key bindings - User:
+    - { "keys": ["ctrl+k", "ctrl+g"], "command": "generate_test"}
+    """
 
     def get_class_name(self, current_file):
+        """
+        Parsers the PHP class name of the current file
+
+        Open the console and run 'view.file_name()' to know the current file.
+        """
         region = sublime.Region(0, self.view.size())
         contents = self.view.substr(region)
         handle = open(current_file, "r")
@@ -38,6 +64,9 @@ class GenerateTestCommand(sublime_plugin.TextCommand):
         return namespace + '\\' + class_name
 
     def run(self, edit):
+        """
+        Called when sublime invoke generate_test command
+        """
         settings = Settings(self.view)
 
         folders = self.view.window().folders()
@@ -50,8 +79,10 @@ class GenerateTestCommand(sublime_plugin.TextCommand):
 
         for folder in folders:
             if current_file.startswith(folder):
+                # parse the PHP class name
                 class_name = self.get_class_name(current_file)
 
+                # create dir tree before trying to write test file
                 relative_path = current_file.replace(folder, '')
                 test_file = relative_path.replace('.php', 'Test.php')
                 test_fullpath = folder + '/' + tests_path + test_file
@@ -59,6 +90,7 @@ class GenerateTestCommand(sublime_plugin.TextCommand):
 
                 subprocess.call(r'mkdir -p "%s"' % (dirname,), shell=True)
 
+                # render phpunit-skelgen command
                 cmd = r'%s %s --verbose --bootstrap="%s" "%s" "%s" "%s" "%s"' % (
                     skeleton_bin,
                     'generate-test',
